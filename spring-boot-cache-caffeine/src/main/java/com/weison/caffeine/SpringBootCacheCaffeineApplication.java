e9e104a6c75a5f411936d1368c9d1ea93b6b8bbc
WeisonWei <weixx3@126.com>
2020-08-25 08:44:07 +0800

    init

---
+++ spring-boot-cache-caffeine/src/main/java/com/weison/caffeine/SpringBootCacheCaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.weison.caffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheCaffeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheCaffeineApplication.class, args);
+    }
+
+}
---
+++ spring-boot-cache-caffeine/src/test/java/com/weison/caffeine/SpringBootCacheCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weison.caffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCacheCaffeineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
